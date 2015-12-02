24d44c70fae0090053228a17b0cda15a2a91352c
Matt Dieselman <dieselmanm@gmail.com>
2015-12-02 12:44:14 -0500

    Worked on getting towers to track enemies Worked on towers shooting bullet prefabs when enemies are in their zone Bullets "hit" enemies and are deleted Need to work on possibility of more than on enemy in zone.. How will that be handled?

---
+++ 7 Deadly Sins Tower Defense/Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class GameManagerScript : MonoBehaviour {
+    private List<GameObject> enemies;
+    private GameObject[] enemiesUnsorted;
+	// Use this for initialization
+	void Start () {
+	    enemies = new List<GameObject>();
+        if(enemiesUnsorted==null){
+            enemiesUnsorted=GameObject.FindGameObjectsWithTag("Monster");
+
+        }
+        for (int i = 0; i < enemiesUnsorted.Length; i++ )
+        {
+            for (int j = 0; j < enemiesUnsorted.Length; j++)
+            {
+                //Debug.Log(System.String.Format("" + unsortedPath[j]));
+
+                if ((enemiesUnsorted[j].GetComponent<MonsterScript>().monsterNumber) == i) { 
+                    enemies.Add(enemiesUnsorted[j]); 
+                }
+
+            }
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ 7 Deadly Sins Tower Defense/Assets/Scripts/MonsterScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterScript : MonoBehaviour {
+    public int monsterNumber;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
